a4e9c91cca6a78cc6a01fc45daf87fe40f62999f
a161120 <david.tarvin@physiciansmutual.com>
2023-02-09 12:21:41 -0600

    update changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <input type="text"
+            placeholder="Enter shipping address"
+            [(ngModel)]="shippingAddress">
+    <button (click)="shippingAddress='123 Main Street'">Set Default Address</button>
+    <p>The shipping address is {{ shippingAddress }}</p>
+  `
 })
 export class AppComponent {
-  title = 'two-way-binding';
+  shippingAddress: string;
 }
